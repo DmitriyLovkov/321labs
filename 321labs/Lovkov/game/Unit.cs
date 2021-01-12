ce28ef12e3a3041d923cdcaa55bbd5b6fef8a828
dmitriylovkov <anisonn@bk.ru>
2021-01-12 15:59:53 +0300

    game 1 start

---
+++ 321labs/Lovkov/game/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Lovkov.game
+{
+    class Unit
+    {
+        int Armor;
+        int Damage;
+        int Range;
+        int Sigh;
+        int Speed;
+
+    }
+}
